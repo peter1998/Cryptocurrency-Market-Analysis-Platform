44c36a74dbbf50a8449285a4163b49fc122851d1
Peter Matov <pmatov@gmail.com>
2023-06-02 22:08:22 +0300

    [S] enhancement of machine learning model

--- machine_learning.py
+++ machine_learning.py
@@ -1,16 +1,52 @@
-from sklearn.model_selection import train_test_split
+from sklearn import model_selection
+import matplotlib.pyplot as plt
+
 from sklearn.linear_model import LinearRegression
-import numpy as np
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.svm import SVR
 
 
-def train_and_predict(df):
-    target_variable = 'current_price'
-    X = df.select_dtypes(include=[np.number]).drop(
-        target_variable, axis=1)  # Only include numeric columns
-    y = df[target_variable]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42)
+def compare_models(X_train, y_train):
+    # prepare configuration for cross validation test harness
+    seed = 7
+
+    # prepare models
+    models = []
+    models.append(('LR', LinearRegression()))
+    models.append(('DTR', DecisionTreeRegressor(random_state=seed)))
+    models.append(('KNR', KNeighborsRegressor()))
+    models.append(('SVR', SVR()))
+
+    # evaluate each model in turn
+    results = []
+    names = []
+    scoring = 'neg_mean_squared_error'
+    for name, model in models:
+        kfold = model_selection.KFold(
+            n_splits=10, random_state=seed, shuffle=True)
+        cv_results = model_selection.cross_val_score(
+            model, X_train, y_train, cv=kfold, scoring=scoring)
+        results.append(cv_results)
+        names.append(name)
+        msg = "%s: %f (%f)" % (name, cv_results.mean(), cv_results.std())
+        print(msg)
+
+    # boxplot algorithm comparison
+    fig = plt.figure()
+    fig.suptitle('Algorithm Comparison')
+    ax = fig.add_subplot(111)
+    plt.boxplot(results)
+    ax.set_xticklabels(names)
+    plt.show()
+
+
+def train_and_predict(X_train, y_train, X_test):
+    # Train a linear regression model
     model = LinearRegression()
     model.fit(X_train, y_train)
+
+    # Make predictions on the test set
     predictions = model.predict(X_test)
-    return y_test, predictions
+
+    return predictions
--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-from data_collection import fetch_data
-from data_processing import process_data
-from machine_learning import train_and_predict
+# main.py
 
-data = fetch_data()
+from sklearn.model_selection import train_test_split
+# Import fetch_current_market_data
+from data_collection import fetch_current_market_data
+from data_processing import process_data
+from machine_learning import train_and_predict, compare_models
+
+data = fetch_current_market_data()  # Use fetch_current_market_data
 if data is not None:
     df = process_data(data)
     print("df.head: ", df.head())
-    y_test, predictions = train_and_predict(df)
+    # Split the data into training and test sets
+    X = df.drop('current_price', axis=1)
+    y = df['current_price']
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42)
+    compare_models(X_train, y_train)  # Compare models
+    predictions = train_and_predict(X_train, y_train, X_test)
     print("Actual values:\n", y_test)
     print("Predicted values:\n", predictions)
 else:
